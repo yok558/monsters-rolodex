fb5221834c5595b89a2f1b39fb95b6c1cc143e88
yok558 <yok.box@gmail.com>
2019-07-09 00:52:00 +0300

    Updates

---
+++ precache-manifest.9c7840693b0c656fc7a41ffdb0faf687.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1bc720fe71961b5bdf9aac13c52b59",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "06aa0c501e8cf06c052b",
+    "url": "/monsters-rolodex/static/css/main.61f292a6.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "06aa0c501e8cf06c052b",
+    "url": "/monsters-rolodex/static/js/main.b89f1c3e.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
